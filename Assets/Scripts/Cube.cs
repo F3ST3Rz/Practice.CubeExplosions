d07a0b983220d180df9db25771cdea59a7492df6
Romi <88428016+F3ST3Rz@users.noreply.github.com>
2024-06-19 16:32:58 +0300

    -

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -1,25 +1,51 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Sharer))]
+[RequireComponent(typeof(Sharer), typeof(Exploder))]
 public class Cube : MonoBehaviour
 {
     private Sharer _sharer;
+    private Exploder _exploder;
+    private int _minRangeNewObjects = 2;
+    private int _maxRangeNewObjects = 6;
+    private float _minRangeChanceSplit = 0f;
+    private float _maxRangeChanceSplit = 1f;
+    private float _chanceSplit = 1f;
 
-    private void Awake()
+    private void Start()
     {
         _sharer = GetComponent<Sharer>();
+        _exploder = GetComponent<Exploder>();
     }
 
     public void Destroy()
     {
-        _sharer.ChanceSpawn();
+        TrySeparate();
         GameObject.Destroy(gameObject);
     }
 
     public void ChangeScale(float value)
     {
-        _sharer.ChangeSplit(value);
         transform.localScale /= value;
+    }
+
+    public void ChangeSplit(float value)
+    {
+        _chanceSplit /= value;
+    }
+
+    private void TrySeparate()
+    {
+        if (Random.Range(_minRangeChanceSplit, _maxRangeChanceSplit) <= _chanceSplit)
+        {
+            int countNewObjects = Random.Range(_minRangeNewObjects, _maxRangeNewObjects);
+            List<Rigidbody> explodableObjects = new List<Rigidbody>();
+
+            for (int i = 0; i < countNewObjects; i++)
+                explodableObjects.Add(_sharer.Spawn());
+
+            _exploder.Explode(explodableObjects);
+        }
     }
 }
 
--- Assets/Scripts/Sharer.cs
+++ Assets/Scripts/Sharer.cs
@@ -1,53 +1,26 @@
-using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent (typeof(Exploder))]
+[RequireComponent(typeof(Cube))]
 public class Sharer : MonoBehaviour
 {
-    private int _minRangeNewObjects = 2;
-    private int _maxRangeNewObjects = 6;
-    private float _minRangeChanceSplit = 0f;
-    private float _maxRangeChanceSplit = 1f;
     private float _divider = 2f;
-    private float _chanceSplit = 1f;
-    private Exploder _exploder;
+    private Cube _cube;
 
     private void Start()
     {
-        _exploder = GetComponent<Exploder>();
+        _cube = GetComponent<Cube>();
     }
 
-    public void ChangeSplit(float value)
+    public Rigidbody Spawn()
     {
-        _chanceSplit /= value;
-    }
+        var newCube = Instantiate(_cube);
+        newCube.gameObject.SetActive(true);
+        newCube.enabled = true;
+        newCube.ChangeScale(_divider);
+        newCube.ChangeSplit(_divider);
 
-    public void ChanceSpawn()
-    {
-        if (Random.Range(_minRangeChanceSplit, _maxRangeChanceSplit) <= _chanceSplit)
-        {
-            int countNewObjects = Random.Range(_minRangeNewObjects, _maxRangeNewObjects);
-            List<Rigidbody> explodableObjects = new List<Rigidbody>();
-
-            for (int i = 0; i < countNewObjects; i++)
-                explodableObjects.Add(Spawn());
-
-            _exploder.Explode(explodableObjects);
-        }
-    }
-
-    private Rigidbody Spawn()
-    {
-        if (TryGetComponent(out Cube cube))
-        {
-            var newCube = Instantiate(cube);
-            newCube.gameObject.SetActive(true);
-            newCube.enabled = true;
-            newCube.ChangeScale(_divider);
-
-            if (newCube.TryGetComponent(out Rigidbody rigidbody))
-                return rigidbody;
-        }
+        if (newCube.TryGetComponent(out Rigidbody rigidbody))
+            return rigidbody;
 
         return null;
     }
